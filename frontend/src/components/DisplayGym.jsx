05a623c75ca0ee2ccd9e97894b1c07e8b2dab1e5
eremus <eremus97@hotmail.com>
2024-04-24 19:06:21 +0800

    Update DisplayGym.jsx

--- frontend/src/components/DisplayGym.jsx
+++ frontend/src/components/DisplayGym.jsx
@@ -38,36 +38,38 @@ const DisplayGym = (props) => {
     getGyms();
   }, []);
 
-  const addBook = async () => {
+  const addGym = async () => {
     const res = await fetchData(
-      "/api/books",
+      "/addgym",
       "PUT",
       {
-        title: titleRef.current.value,
-        author: authorRef.current.value,
-        year: yearRef.current.value,
+        gymname: titleRef.current.value,
+        address: authorRef.current.value,
+        openingghours: yearRef.current.value,
       },
-      userCtx.accessToken
+      undefined
+      // userCtx.accessToken
     );
 
     if (res.ok) {
-      getBooks();
+      getGyms();
     } else {
       alert(JSON.stringify(res.data));
       console.log(res.data);
     }
   };
 
-  const deleteBook = async (id) => {
+  const deleteGym = async (id) => {
     const res = await fetchData(
-      "/api/books/" + id,
+      "/deletegym/" + id,
       "DELETE",
       undefined,
-      userCtx.accessToken
+      undefined
+      // userCtx.accessToken
     );
 
     if (res.ok) {
-      getBooks();
+      getGyms();
     } else {
       alert(JSON.stringify(res.data));
       console.log(res.data);
@@ -100,7 +102,7 @@ const DisplayGym = (props) => {
             className="col-md-9"
           ></input>
 
-          <button type="submit" className="col-md-3" onClick={getGame}>
+          <button type="submit" className="col-md-3" onClick={getGyms}>
             Search
           </button>
         </div>
@@ -109,7 +111,7 @@ const DisplayGym = (props) => {
       <div id="gameNames">
         <br />
         {showModal && (
-          <form onSubmit={getGame}>
+          <form onSubmit={getGyms}>
             <div>
               <input
                 type="text"
@@ -124,31 +126,16 @@ const DisplayGym = (props) => {
                 onChange={(e) => setUserGroup(e.target.value)}
               />
               <br></br>
-              <button type="submit" onClick={addGame}>
-                Bought Passes
+              <button type="submit" onClick={addGym}>
+                add gym
               </button>
-              <button type="submit" onClick={addWishlist}>
-                Sell Passes
+              <button type="submit" onClick={deleteGym}>
+                del gym
               </button>
             </div>
           </form>
         )}
         <br />
-        {gameNames.map((game, index) => (
-          <div key={index}>
-            <img src={game.imgUrl} alt={game.name} />
-            <span>{game.name}</span>
-            <button
-              className="col-sm-1"
-              onClick={() => {
-                setSelectedGameName(game.name);
-                setShowModal(true);
-              }}
-            >
-              Add Gym
-            </button>
-          </div>
-        ))}
       </div>
       <br />
       <br />
