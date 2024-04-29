b5afe12912c5d459b6837d2539f1ac84cdf05015
eremus <eremus97@hotmail.com>
2024-04-29 10:50:43 +0800

    Update Main.jsx

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/user"; // Adjust the import path as necessary
 
 const Main = () => {
+  const userCtx = useContext(UserContext);
+
   return (
     <>
       <h1>Welcome to Climbuddy!</h1>
+      <h2>Welcome {userCtx.username}!</h2>
+
       <h2>Here are your remaining passes:</h2>
       <h2>These are the gyms you've went recently:</h2>
     </>
